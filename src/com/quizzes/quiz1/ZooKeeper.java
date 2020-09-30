74d63dec666ab4489cbe475f7663ab83735214aa
wpower12 <willpowe@gmail.com>
2020-09-30 16:27:20 -0400

    Quiz snippets.

--- src/com/quizzes/quiz1/ZooKeeper.java
+++ src/com/quizzes/quiz1/ZooKeeper.java
@@ -3,9 +3,11 @@ package com.quizzes.quiz1;
 public class ZooKeeper {
     void go(){
         Zebra z = new Zebra();
-        Mammal m = (Mammal) z;
-        System.out.println(z.name+z.makeNoise());
-        System.out.println(m.name+m.makeNoise());
+        Mammal m = z;
+        // Fields are accessed based on variable type.
+        // Additionally, the field in the super type 'shadows' the similarly named field in the sub.
+        System.out.println(z.name+z.makeNoise()); // Zebra  fields, Zebra methods
+        System.out.println(m.name+m.makeNoise()); // Mammal fields, Zebra methods
     }
 
     public static void main(String[] args) {
