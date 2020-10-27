03ca10dc06bf7f652751470a98ee3681505e464a
wpower12 <willpowe@gmail.com>
2020-10-27 16:40:51 -0400

    moreeeeeeeeeee

---
+++ src/com/lectures/lect00/Test.java
@@ -0,0 +1,7 @@
+package com.lectures.lect00;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("I'm here to make the projects GUI nest things properly!");
+    }
+}
